a97920c9f627c4584f8e18adc588bb827ff27cab
amir hessam <amirhesamshomaliq@gmail.com>
2024-09-29 21:53:44 +0330

    tamrin aval

--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Salam extends React.Component
+{
+  render()
+  {
+    return(
+      <h1>
+      salma dostan
+       </h1>
+    )
+  }
+}
+
+class Timer extends React.Component
+{
+  render()
+  {
+    return(
+      <h2>
+      it is:{new Date().toLocaleString()}
+    </h2>
+    )
+  }
+}
+
+
+class App extends React.Component
+{
+  render()
+  {
+    return <div>
+      <Salam/>
+      <Timer/>
+
+  </div>
+  }
+}
+
+// var object1=new App();
+
+
+class TIMESharing extends React.Component
+{
+  render()
+  {
+    return (
+      setInterval(()=>
+        {
+          hasel();
+        },1000)
+        
+        
+    )
+  }
+}
+
+
+const hasel=()=>
+{
+  // const element=(
+    // <div>
+    //   <h1>
+    //   salma dostan
+    //    </h1>
+    //   <h2>
+    //     it is:{new Date().toLocaleString()}
+    //   </h2>
+    // </div>
+  // )
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+   <App/>
+  
+  );
+}
+
+<TIMESharing/>
